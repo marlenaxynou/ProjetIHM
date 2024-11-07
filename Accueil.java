910795e7f3daf348a90ccf97cb4a631fd826e125
Steven H. <150437820+Steven200405@users.noreply.github.com>
2024-11-07 17:22:18 +0100

    Ajout des différentes pages

---
+++ Accueil.java
@@ -0,0 +1,11 @@
+package application;
+
+public class Accueil extends Application {
+
+    @Override
+    public void start(Stage stage) {
+     }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
